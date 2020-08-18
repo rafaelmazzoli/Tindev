d7d0cfb7f39d5b5a15c1ba2cb4ca240c18d80476
Rafael Mazzoli Camargo <rafaelmazzoli@hotmail.com>
2020-08-18 12:43:34 -0300

    first commit

---
+++ backend/src/controllers/Dev.js
@@ -0,0 +1,49 @@
+const { Types } = require("mongoose");
+const axios = require("axios");
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async index(request, response) {
+    const { user } = request.headers;
+
+    const loggedDev = await Dev.findById(Types.ObjectId(user));
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        {
+          _id: {
+            $nin: loggedDev.likes.map((liked) => Types.ObjectId(liked)),
+          },
+        },
+        {
+          _id: {
+            $nin: loggedDev.dislikes.map((disliked) =>
+              Types.ObjectId(disliked)
+            ),
+          },
+        },
+      ],
+    });
+
+    return response.json(users);
+  },
+
+  async store(request, response) {
+    const { username } = request.body;
+
+    const userExists = await Dev.findOne({ user: username });
+
+    if (userExists) return response.json(userExists);
+
+    const responseGit = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+
+    const { name, bio, avatar_url: avatar } = responseGit.data;
+
+    const dev = await Dev.create({ name, user: username, bio, avatar });
+
+    return response.json(dev);
+  },
+};
